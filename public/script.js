d26b0eadcbb17e5e75c34ad7eb4887d84cde748e
yanir2007 <87027341+godmingty@users.noreply.github.com>
2022-09-02 19:24:10 +0300

    noteget - time to refrash
    note get update #2

--- public/script.js
+++ public/script.js
@@ -1,26 +1,64 @@
 window.addEventListener("DOMContentLoaded",() => {
-	let n = new Notifications("#notifications");
+	var notifications = 1
+	var the_cookie = getCookie("meseges")
+	if (document.cookie == ""){
+		document.cookie = `meseges=${0}`;
+		the_cookie = getCookie("meseges")
+	}else{
+		the_cookie = getCookie("meseges")
+		if (notifications == the_cookie){
+			notifications = 0
+		}else{
+			notifications = notifications - the_cookie
+		}
+	}
+	let n = new Notifications("#notifications",notifications);
 });
 
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+	  let c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+  }
+
+
 class Notifications {
-	constructor(qs) {
+	constructor(qs,notifications_n) {
 		this.el = document.querySelector(qs);
 		this.badge = null;
-		this.items = 0;
+		this.items = notifications_n;
 		this.timeout = null;
 		this.init();
 	}
-	init(val) {
+	init() {
 		if (this.el) {
-			console.log("new meesege");
-			this.el.addEventListener("click",this.read.bind(this));
+			this.el.addEventListener("click",() => {
+				document.cookie = `meseges=${this.items}`
+				this.read()
+			});
 			this.badge = this.el.querySelector("[data-badge]");
 
-			this.reset(val||1);
+
+			if (this.items != 0) {
+				this.reset(this.items||1);
+			}else{
+				this.read()
+			}
 		}
 	}
 	read() {
-		if (this.items > 0) {
+		if (this.items > -1) {
 			this.items = 0;
 			this.el.classList.remove("notifications--active");
 			this.badge.innerHTML = "";
